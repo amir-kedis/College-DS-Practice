c25bc42abe3728ff8c46fa57718ad7e232826c5b
amir-kedis <amir.kedis@gmail.com>
2023-02-26 19:56:57 +0200

    prepare basic driver file

--- STACK-ADT/testing-stack.cpp
+++ STACK-ADT/testing-stack.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <string>
+
+#include "StackInterface.h"
+#include "ArrayStack.h"
 
 using namespace std;
 
 int main()
 {
+	StackInterface<string>* stackPtr;
+
+	char userChoice;
+
+	cout << "Which implementation would you like to test ('A' || 'L'): ";
+	cin >> userChoice;
+
+	if (userChoice == 'A')
+	{
+		// stackPtr = new ArrayStack<string>();
+		cout << "Testing the Array-Based Stack:\n";
+
+	}
+	else {
+		// stackPtr = new LinkedStack<string>();
+		cout << "Testing the LinkedList-Based Stack:\n";
+	}
 
 	return 0;
 }
