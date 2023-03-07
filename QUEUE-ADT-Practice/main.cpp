f16ddfac68140ad6f56126ba07a34ad3ef5590d2
amir-kedis <amir.kedis@gmail.com>
2023-03-07 16:39:01 +0200

    prepare main testing file

--- QUEUE-ADT-Practice/main.cpp
+++ QUEUE-ADT-Practice/main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include "QueueInterface.h"
 
 using namespace std;
 
+void displayQueue(QueueInterface<string>* queuePtr);
+void queueTester(QueueInterface<string>* queuePtr);
+
 int main()
 {
+	QueueInterface<string>* queuePtr;
+	char userChoice;
 
+	cout << "Which implementation would you like to test ('A' || 'L'): ";
+	cin >> userChoice;
+
+	if (userChoice == 'A')
+	{
+		// queuePtr = new ArrayQueue<string>();
+		cout << "Testing the Array-Based Stack:\n";
+	}
+	else {
+		// queuePtr = new LinkedQueue<string>();
+		cout << "Testing the LinkedList-Based Stack:\n";
+	}
+
+	cout << "The initial stack is empty.\n\n";
+
+	// starts the tests
+	queueTester(queuePtr);
+
+	delete queuePtr;
+	cout << "All done!\n";
+}
+
+void displayQueue(QueueInterface<string>* queuePtr)
+{
+}
+
+void queueTester(QueueInterface<string>* queuePtr)
+{
 }
