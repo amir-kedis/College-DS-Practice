df5b3b09a6d1132aa518d1002c2c3a70b0ae3fe1
amir-kedis <amir.kedis@gmail.com>
2023-03-07 18:28:36 +0200

    implement array queue

--- QUEUE-ADT-Practice/ArrayQueue.h
+++ QUEUE-ADT-Practice/ArrayQueue.h
@@ -1,11 +1,19 @@
 #pragma once
 #include "QueueInterface.h"
+
+const int QUEUE_SIZE = 50;
+
 template <class ItemType>
 class ArrayQueue :
 	public QueueInterface<ItemType>
 {
+	/// QUEUE Implementation using circular array
+	/// I recommend you try to make it before 
 private:
-
+	ItemType items[QUEUE_SIZE];
+	int front;
+	int rear;
+	int count;
 public:
 	ArrayQueue();
 	virtual bool isEmpty() const;
@@ -17,35 +25,66 @@ public:
 
 template<class ItemType>
 inline ArrayQueue<ItemType>::ArrayQueue()
+	:front(-1), rear(-1), count(0)
 {
 }
 
 template<class ItemType>
 inline bool ArrayQueue<ItemType>::isEmpty() const
 {
-	return false;
+	return count == 0;
 }
 
 template<class ItemType>
 inline bool ArrayQueue<ItemType>::enqueue(const ItemType& newEntry)
 {
-	return false;
+	if (count >= QUEUE_SIZE)
+	{
+		return false;
+	}
+
+	if (count == 0)
+	{
+		front = (front + 1) % QUEUE_SIZE;
+	}
+
+	rear = (rear + 1) % QUEUE_SIZE; // move the back index clockwise 
+	items[rear] = newEntry;			// put item in new position
+	count++;						// increase the count
+
+	return true;
 }
 
 template<class ItemType>
 inline bool ArrayQueue<ItemType>::dequeue()
 {
-	return false;
+	if (isEmpty())
+	{
+		return false;
+	}
+
+	front = (front + 1) % QUEUE_SIZE; // move the front index clockwise (freeing its place)
+	count--;
+
+	return true;
 }
 
 template<class ItemType>
 inline ItemType ArrayQueue<ItemType>::peekFront() const
 {
-	return ItemType();
+	return items[front];
 }
 
 template<class ItemType>
 inline std::vector<ItemType> ArrayQueue<ItemType>::toVector() const
 {
-	return std::vector<ItemType>();
+	std::vector<ItemType> queueItems;
+	int index;
+	for (int i = 0; i < count; i++)
+	{
+		index = (front + i) % QUEUE_SIZE;
+		queueItems.push_back(items[index]);
+	}
+
+	return queueItems;
 }
